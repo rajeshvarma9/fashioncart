49a4beb85361d76f4a95d8662cb651b2e42be3b7
rajesh <rajeshvarma4195@gmail.com>
2025-06-19 13:44:58 -0700

    Initial commit

---
+++ src/main/java/com/Fashioncart/FashioncartApplication.java
@@ -0,0 +1,13 @@
+package com.Fashioncart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashioncartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FashioncartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Fashioncart/FashioncartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Fashioncart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashioncartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
